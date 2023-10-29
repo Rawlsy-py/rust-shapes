7a99e1d4452fb1669dc5ec12f0c592513cd14bb3
Rawlsy-py <christopher_m_mitchell@live.co.uk>
2023-10-29 12:34:50 +0000

    guessing game

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use rand::Rng;
 use std::io;
 
 fn main() {
@@ -5,12 +6,15 @@ fn main() {
 
     println!("Please input your guess.");
 
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
     let mut guess = String::new();
 
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line");
 
+    println!("The secret number is: {secret_number}");
+
     println!("You guessed: {guess}")
 }
-
