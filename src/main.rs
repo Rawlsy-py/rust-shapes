5c0c4dfd05762a737c7d4cc76ef12049bfbe5271
Rawlsy-py <christopher_m_mitchell@live.co.uk>
2023-11-25 13:47:11 +0000

    User selection of shape

--- src/main.rs
+++ src/main.rs
@@ -7,24 +7,32 @@ fn main() {
     // Welcome Message
     println!("Welcome to Rust Shapes Calculator");
 
-    let mut input = String::new();
+    println!("What Shape would you like to calculate the area of?");
+
+    let mut selection: String = String::new();
 
     io::stdin()
-        .read_line(&mut input)
+        .read_line(&mut selection)
         .expect("Failed to read line");
 
-    let input = input.trim();
+    if selection == "Circle" {
+        let mut input = String::new();
 
-    let number: f64 = match input.parse() {
-        Ok(num) => num,
-        Err(_) => {
-            println!("Please enter a valid number");
-            return;
-        }
-    };
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read line");
 
-    let radius = number;
+        let input = input.trim();
 
-    let area = circle::calculate_area(radius);
-    println!("Area of the circle: {}", area);
+        let number: f64 = match input.parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please enter a valid number");
+                return;
+            }
+        };
+    }
+    else {
+        println!("Thank you, Have a nice day!")
+    }
 }
